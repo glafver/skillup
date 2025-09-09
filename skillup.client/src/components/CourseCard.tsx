2b160029056f304c471678a3e6a6c2720f133c5b
OlleWork <simonsenolle@gmail.com>
2025-09-09 16:25:29 +0200

    Lagt till redirection för knapparna i Courses

--- skillup.client/src/components/CourseCard.tsx
+++ skillup.client/src/components/CourseCard.tsx
@@ -1,12 +1,25 @@
 import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 
 const imageMap = import.meta.glob("../images/*", { eager: true, as: "url" }) as Record<string, string>;
 
+
+
 export const CourseCard = ({title, description, imageUrl}: {title: string, description: string, imageUrl: string}) => {
+    const navigate = useNavigate();
+  
     const src = useMemo(() => {
+      
     return imageMap[`../images/${imageUrl}`];
   }, [imageUrl]);
+
+    const titleToSlug: Record<string, string> = {
+    "C#": "csharp",
+    "Python": "python",
+    "JavaScript": "javascript",
+    "HTML": "html"
+  }
 
   return (
     // Cards vertikalt
@@ -35,7 +48,14 @@ export const CourseCard = ({title, description, imageUrl}: {title: string, descr
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
-      <button className="flex-none self-center my-2 mx-4 px-4 py-2 text-sm font-medium rounded bg-cyan-700 text-white hover:bg-teal-700 transition transform hover:scale-105">
+      <button 
+        onClick={() => {
+          const slug = titleToSlug[title];
+          if (slug) {
+            navigate(`/courses/${slug}`);
+          }
+        }}
+        className="flex-none self-center my-2 mx-4 px-4 py-2 text-sm font-medium rounded bg-cyan-700 text-white hover:bg-teal-700 transition transform hover:scale-105">
         Start Course
       </button>
     </div>
